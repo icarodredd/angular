540c1c29fa67c90091751e104268b564286dc0dd
icarodredd <icarog854@gmail.com>
2024-10-03 10:38:38 -0300

    displaying fake data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DUMMY_USERS } from "../dummy-users";
 
 @Component({
-  selector: 'app-user',
+  selector: "app-user",
   standalone: true,
   imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  templateUrl: "./user.component.html",
+  styleUrl: "./user.component.css",
 })
 export class UserComponent {
-
+  users = DUMMY_USERS;
 }
