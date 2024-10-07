a7cca363678c728ffae66080b6476e3464f0c5c1
icarodredd <icarog854@gmail.com>
2024-10-07 15:00:21 -0300

    showing a name of the user selected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from "./dummy-users";
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
   title = "angular";
   users = DUMMY_USERS;
+  id = "";
 
   onSelectUser(id: string) {
-    console.log(id);
+    this.id = id;
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { DUMMY_USERS } from "../dummy-users";
 
 @Component({
-  selector: 'app-tasks',
+  selector: "app-tasks",
   standalone: true,
   imports: [],
-  templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  templateUrl: "./tasks.component.html",
+  styleUrl: "./tasks.component.css",
 })
 export class TasksComponent {
+  @Input({ required: true }) id!: string;
 
+  get name() {
+    return DUMMY_USERS.find((user) => user.id === this.id)?.name;
+  }
 }
