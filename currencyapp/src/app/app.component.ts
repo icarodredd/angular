6d80bdfdb31a8ed42b40b2156ef2271371888015
icarodredd <icarog854@gmail.com>
2024-10-14 14:31:29 -0300

    calculate on submit

--- currencyapp/src/app/app.component.ts
+++ currencyapp/src/app/app.component.ts
@@ -8,4 +8,31 @@ import { UserInputComponent } from "./user-input/user-input.component";
   templateUrl: "./app.component.html",
   imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  calculateInvestmentResults(data: {
+    initialInvestment: number;
+    annualInvestment: number;
+    expectedReturn: number;
+    duration: number;
+  }) {
+    const annualData = [];
+    let investmentValue = data.initialInvestment;
+
+    for (let i = 0; i < data.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (data.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + data.annualInvestment;
+      const totalInterest = investmentValue - data.annualInvestment * year - data.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: data.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
--- currencyapp/src/app/user-input/user-input.component.ts
+++ currencyapp/src/app/user-input/user-input.component.ts
@@ -21,33 +21,6 @@ export class UserInputComponent {
     duration: number;
   }>();
 
-  calculateInvestmentResults(
-    initialInvestment: number,
-    annualInvestment: number,
-    expectedReturn: number,
-    duration: number,
-  ) {
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    return annualData;
-  }
-
   onSubmit() {
     this.calculate.emit({
       initialInvestment: +this.initialInvestment,
