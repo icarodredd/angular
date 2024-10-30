e325510f72688adda324f038e0d9f92f5d14c171
icarodredd <icarog854@gmail.com>
2024-10-30 14:03:05 -0300

    datePipe and decimalPipe

--- pipes-deepdive/src/app/app.component.ts
+++ pipes-deepdive/src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { DatePipe, DecimalPipe } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  templateUrl: './app.component.html',
+  templateUrl: "./app.component.html",
+  imports: [DatePipe, DecimalPipe],
 })
 export class AppComponent {
   currentDate = new Date();
@@ -14,9 +16,7 @@ export class AppComponent {
     chicago: 65.0775238,
   };
 
-  historicTemperatures = [
-    25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
-  ];
+  historicTemperatures = [25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5];
 
   onReset(index: number) {
     this.historicTemperatures[index] = 18;
