81fa410f9ec9bfb6d9cc0603e6f1f69b10aa1bd3
icarodredd <icarog854@gmail.com>
2024-11-01 14:55:52 -0300

    injecting on new-task

--- services-deepdive/src/app/tasks/new-task/new-task.component.ts
+++ services-deepdive/src/app/tasks/new-task/new-task.component.ts
@@ -1,17 +1,22 @@
-import { Component, ElementRef, viewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ElementRef, inject, viewChild } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { TasksService } from "../tasks.service";
 
 @Component({
-  selector: 'app-new-task',
+  selector: "app-new-task",
   standalone: true,
   imports: [FormsModule],
-  templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css',
+  templateUrl: "./new-task.component.html",
+  styleUrl: "./new-task.component.css",
 })
 export class NewTaskComponent {
-  private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+  constructor() {}
+  private tasksService = inject(TasksService);
+
+  private formEl = viewChild<ElementRef<HTMLFormElement>>("form");
 
   onAddTask(title: string, description: string) {
+    this.tasksService.addTasks({ title, description });
     this.formEl()?.nativeElement.reset();
   }
 }
