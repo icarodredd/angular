2748a1ae7ba5eb5a5cd2ec0b800e22dbf887ef7f
icarodredd <icarog854@gmail.com>
2024-11-04 14:53:07 -0300

    fetching and displaying a fallback

--- requests/src/app/places/available-places/available-places.component.ts
+++ requests/src/app/places/available-places/available-places.component.ts
@@ -1,16 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit, signal } from "@angular/core";
 
-import { Place } from '../place.model';
-import { PlacesComponent } from '../places.component';
-import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { Place } from "../place.model";
+import { PlacesComponent } from "../places.component";
+import { PlacesContainerComponent } from "../places-container/places-container.component";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-available-places',
+  selector: "app-available-places",
   standalone: true,
-  templateUrl: './available-places.component.html',
-  styleUrl: './available-places.component.css',
+  templateUrl: "./available-places.component.html",
+  styleUrl: "./available-places.component.css",
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFecthing = signal(true);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    const subscription = this.httpClient.get<{ places: Place[] }>("http://localhost:3000/places").subscribe({
+      next: (res) => this.places.set(res.places),
+      complete: () => this.isFecthing.set(false),
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
